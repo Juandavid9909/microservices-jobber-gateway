7cd1959b67940dfbf531a6052e81b3a72c95e786
Juan David <juandavid918@gmail.com>
2024-11-06 11:10:52 -0500

    Método para verificar el email y token

--- src/controllers/auth/verify-email.ts
+++ src/controllers/auth/verify-email.ts
@@ -3,11 +3,10 @@ import { AxiosResponse } from "axios";
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
-export class SignIn {
-  public async read(req: Request, res: Response): Promise<void> {
-    const response: AxiosResponse = await authService.signIn(req.body);
+export class VerifyEmail {
+  public async update(req: Request, res: Response): Promise<void> {
+    const response: AxiosResponse = await authService.verifyEmail(req.body.token);
 
-    req.session = { jwt: response.data.token };
     res.status(StatusCodes.OK).json({
       message: response.data.message,
       user: response.data.user
