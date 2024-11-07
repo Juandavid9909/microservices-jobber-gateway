eb0989699667d466a214fee8020b7a87d9165cf1
Juan David <juandavid918@gmail.com>
2024-11-07 11:04:34 -0500

    Método para refrescar el token

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,5 +1,6 @@
 import { authMiddleware } from "@gateway/services/auth-middleware";
 import { CurrentUser } from "@gateway/controllers/auth/current-user";
+import { Refresh } from "@gateway/controllers/auth/refresh-token";
 import express, { Router } from "express";
 
 class CurrentUserRoutes {
@@ -10,6 +11,7 @@ class CurrentUserRoutes {
   }
 
   public routes(): Router {
+    this.router.get("/auth/refresh-token/:username", authMiddleware.checkAuthentication, Refresh.prototype.token);
     this.router.get("/auth/currentUser", authMiddleware.checkAuthentication, CurrentUser.prototype.read);
     this.router.post("/auth/resend-email", authMiddleware.checkAuthentication, CurrentUser.prototype.resendEmail);
 
