b199da36b69d6dd385c3d6b4675789f6ce37657c
Juan David <juandavid918@gmail.com>
2024-12-04 15:21:56 -0500

    Se agrega controlador para search de auth

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,13 @@ import { authMiddleware } from "@gateway/services/auth-middleware";
 import { authRoutes } from "@gateway/routes/auth";
 import { currentUserRoutes } from "@gateway/routes/current-user";
 import { healthRoutes } from "@gateway/routes/health";
+import { searchRoutes } from "@gateway/routes/search";
 
 const BASE_PATH = "/api/gateway/v1";
 
 export const appRoutes = (app: Application) => {
   app.use("", healthRoutes.routes());
   app.use(BASE_PATH, authRoutes.routes());
+  app.use(BASE_PATH, searchRoutes.routes());
   app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
 };
